c0793592edebec0b8e31e569c3ea27a3c301d47e
Nam Trung Truong <codehappyness@gmail.com>
2026-01-28 22:18:41 +0700

    Update learning

--- databases/script.sql
+++ databases/script.sql
@@ -1,27 +1,20 @@
--- MySQL dump 10.13  Distrib 9.6.0, for macos14.8 (x86_64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-12.1.2-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: ptudwebdb
 -- ------------------------------------------------------
--- Server version	9.6.0
+-- Server version	12.1.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
-
---
--- GTID state at the beginning of the backup 
---
-
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '66b3a408-fc1d-11f0-afba-a224ca8c5c29:1-24';
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
 
 --
 -- Table structure for table `loaitin`
@@ -29,13 +22,13 @@ SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '66b3a408-fc1d-11f0-afba-a224ca8c5c29:1-
 
 DROP TABLE IF EXISTS `loaitin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `loaitin` (
-  `maloai` int NOT NULL AUTO_INCREMENT,
+  `maloai` int(11) NOT NULL AUTO_INCREMENT,
   `tenloai` varchar(100) NOT NULL,
-  `trangthai` int NOT NULL DEFAULT '1',
+  `trangthai` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`maloai`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,9 +37,17 @@ CREATE TABLE `loaitin` (
 
 LOCK TABLES `loaitin` WRITE;
 /*!40000 ALTER TABLE `loaitin` DISABLE KEYS */;
-INSERT INTO `loaitin` VALUES (1,'Van hoa',1),(2,'Xa hoi',1),(3,'Kinh te',1),(4,'Chinh tri',1),(5,'Giao duc',1),(6,'Y te',1);
+set autocommit=0;
+INSERT INTO `loaitin` VALUES
+(1,'Van hoa',1),
+(2,'Xa hoi',1),
+(3,'Kinh te',1),
+(4,'Chinh tri',1),
+(5,'Giao duc',1),
+(6,'Y te',1);
 /*!40000 ALTER TABLE `loaitin` ENABLE KEYS */;
 UNLOCK TABLES;
+commit;
 
 --
 -- Table structure for table `users`
@@ -54,16 +55,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `type` enum('member','admin') NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,10 +73,13 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Trung','$2y$12$p2W/aIYJ8dRqXpN9x3nzfe4Y34L5X6Picnomc5EInKZjonCNytKCK','tr@gmail.com','member','2025-12-06 00:46:39'),(2,'admin','$2y$12$oYC2Ccu1ocOAVpi1Vt6RQ.WQy6zpBKJk6U8OLPZAUsgcDQebPqePi','admin@gmail.com','member','2026-01-21 14:28:02');
+set autocommit=0;
+INSERT INTO `users` VALUES
+(1,'Trung','$2y$12$p2W/aIYJ8dRqXpN9x3nzfe4Y34L5X6Picnomc5EInKZjonCNytKCK','tr@gmail.com','member','2025-12-06 00:46:39'),
+(2,'admin','$2y$12$oYC2Ccu1ocOAVpi1Vt6RQ.WQy6zpBKJk6U8OLPZAUsgcDQebPqePi','admin@gmail.com','member','2026-01-21 14:28:02');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+commit;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -84,6 +88,6 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2026-01-28 21:49:17
+-- Dump completed on 2026-01-28 22:18:27
